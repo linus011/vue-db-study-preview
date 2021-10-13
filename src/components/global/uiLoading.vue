4a17136fffd93583921c4bc041b158e2e50f3711
anoju <zun5761@gmail.com>
2021-10-13 13:59:27 +0900

    로딩창을 컴포넌트로 만들어서 쓰기

---
+++ src/components/global/uiLoading.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    v-if="open"
+    class="loading-wrap"
+  >
+    <div class="tl">
+      <div>
+        <div
+          class="loading-icon"
+          role="img"
+          aria-label="화면을 불러오는중입니다."
+        >
+          <div />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'UiLoading',
+  props: {
+    open: { type: Boolean, default: false },
+  },
+  data() {
+    return {
+    }
+  },
+}
+</script>
--- src/views/guide/index.vue
+++ src/views/guide/index.vue
@@ -7,7 +7,7 @@
             class="button line"
             @click="isLoading = true"
           >
-            로딩창 열기
+            로딩창(마크업) 열기
           </button>
         </div>
         <div>
@@ -16,7 +16,29 @@
             style="z-index: 501"
             @click="isLoading = false"
           >
-            로딩창 닫기
+            로딩창(마크업) 닫기
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="btn-wrap">
+      <div class="flex">
+        <div>
+          <button
+            class="button line"
+            @click="isLoading2 = true"
+          >
+            로딩창(컴포넌트) 열기
+          </button>
+        </div>
+        <div>
+          <button
+            class="button line"
+            style="z-index: 501"
+            @click="isLoading2 = false"
+          >
+            로딩창(컴포넌트) 닫기
           </button>
         </div>
       </div>
@@ -59,6 +81,7 @@
     <h1>가나다라마바사</h1>
     <h1>가나다라마바사</h1>
 
+    <!-- 마크업 로딩창 -->
     <div
       v-if="isLoading"
       class="loading-wrap"
@@ -75,20 +98,28 @@
         </div>
       </div>
     </div>
+    <!--// 마크업 로딩창 -->
+
+    <!-- 컴포넌트 로딩창 -->
+    <ui-loading :open="isLoading2" />
+    <!-- // 컴포넌트 로딩창 -->
   </ui-page>
 </template>
 
 <script>
 import uiPage from '@/components/global/uiPage.vue'
+import UiLoading from '@/components/global/uiLoading.vue'
 
 export default {
   name: 'Index',
   components: {
     uiPage,
+    UiLoading,
   },
   data() {
     return {
       isLoading: false,
+      isLoading2: false,
     }
   },
 }
