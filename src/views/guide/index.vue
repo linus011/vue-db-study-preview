7e5d78d9157768ffc04dd999518df5ca097f3109
anoju <zun5761@gmail.com>
2021-10-13 13:52:04 +0900

    초간단 로딩창  (페이지에 넣고 바로 띄우기)

--- src/views/guide/index.vue
+++ src/views/guide/index.vue
@@ -1,5 +1,26 @@
 <template>
   <ui-page title="메인화면">
+    <div class="btn-wrap">
+      <div class="flex">
+        <div>
+          <button
+            class="button line"
+            @click="isLoading = true"
+          >
+            로딩창 열기
+          </button>
+        </div>
+        <div>
+          <button
+            class="button line"
+            style="z-index: 501"
+            @click="isLoading = false"
+          >
+            로딩창 닫기
+          </button>
+        </div>
+      </div>
+    </div>
     <h1>가나다라마바사</h1>
     <h1>가나다라마바사</h1>
     <h1>가나다라마바사</h1>
@@ -37,6 +58,23 @@
     <h1>가나다라마바사</h1>
     <h1>가나다라마바사</h1>
     <h1>가나다라마바사</h1>
+
+    <div
+      v-if="isLoading"
+      class="loading-wrap"
+    >
+      <div class="tl">
+        <div>
+          <div
+            class="loading-icon"
+            role="img"
+            aria-label="화면을 불러오는중입니다."
+          >
+            <div />
+          </div>
+        </div>
+      </div>
+    </div>
   </ui-page>
 </template>
 
@@ -47,6 +85,11 @@ export default {
   name: 'Index',
   components: {
     uiPage,
+  },
+  data() {
+    return {
+      isLoading: false,
+    }
   },
 }
 </script>
